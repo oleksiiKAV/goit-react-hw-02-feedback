bf1d8159cf0cc8221d03c8ae00937c37a0a900e9
oleksiiKAV <43839148+oleksiiKAV@users.noreply.github.com>
2023-05-28 19:52:15 +0300

    fixed

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -3,5 +3,7 @@ import { Mess } from 'components/Statistics/Statistics.styled';
 function Notification({ message }) {
   return <Mess>{message}</Mess>;
 }
-
+Notification.protoTypes = {
+  message: PT.string,
+};
 export default Notification;
