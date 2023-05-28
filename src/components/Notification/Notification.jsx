2c920524a3e8c6c7865272642063a62105f28451
oleksiiKAV <43839148+oleksiiKAV@users.noreply.github.com>
2023-05-28 07:23:30 +0000

    Deploying to gh-pages from @ oleksiiKAV/goit-react-hw-02-feedback@d65bd30fa2de85ac554b294659ad0dfdecd95bca 🚀

--- src/components/Notification/Notification.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Mess } from 'components/Statistics/Statistics.styled';
-function Notification({ message }) {
-  return <Mess>{message}</Mess>;
-}
-
-export default Notification;
