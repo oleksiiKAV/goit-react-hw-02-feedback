47a01a7cfbc3b12d9aec595c5c5d69e0e7898d1c
oleksiiKAV <43839148+oleksiiKAV@users.noreply.github.com>
2023-05-28 10:12:10 +0300

    feedback

---
+++ src/components/App.jsx
@@ -0,0 +1,65 @@
+import { Component } from 'react';
+import { Statistics, FeedbackOptions, Section } from '../components';
+import Notification from './Notification/Notification';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = e => {
+    const { name } = e.target;
+    this.setState(prevState => ({ [name]: prevState[name] + 1 }));
+  };
+
+  countTotalFeedback() {
+    return this.state.good + this.state.neutral + this.state.bad;
+  }
+
+  countPositiveFeedbackPercentage(sum) {
+    return (Math.round((this.state.good / sum) * 100) || 0) + '%';
+  }
+
+  render() {
+    const sum = this.countTotalFeedback();
+    const persentGood = this.countPositiveFeedbackPercentage(sum);
+
+    return (
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={this.state}
+            onLeaveFeedback={this.onLeaveFeedback}
+          />
+        </Section>
+        <Section title="Statistics">
+          {sum === 0 ? (
+            // <p>There is no feedback</p>
+            <Notification message="There is no feedback" />
+          ) : (
+            <Statistics
+              Good={this.state.good}
+              Neutral={this.state.neutral}
+              Bad={this.state.bad}
+              Total={sum}
+              PositiveFeadback={persentGood}
+            />
+          )}
+        </Section>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Mess } from 'components/Statistics/Statistics.styled';
+function Notification({ message }) {
+  return <Mess>{message}</Mess>;
+}
+
+export default Notification;
